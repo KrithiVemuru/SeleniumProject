38074dffa49384a5bd1b9a40df8763c19b3ca6a3
KrithiVemuru <105637943+KrithiVemuru@users.noreply.github.com>
2022-09-05 17:01:27 +0530

    Update TestRunner.java

--- src/test/java/TestRunner.java
+++ src/test/java/TestRunner.java
@@ -4,6 +4,6 @@ import io.cucumber.junit.CucumberOptions;
 
 @RunWith(Cucumber.class)
 @CucumberOptions(features = {
-		"src/test/resources/features/AssignmentScenarios.feature" }, plugin = {"pretty", "html:target/cucumber-report/index.html" })
+		"src/test/resources/features/AssignmentScenarios.feature" }, plugin = {"pretty", "html:target/cucumber-report/index.html" ,"json:target/cucumber-report/report.json"})
 public class TestRunner {
 }
